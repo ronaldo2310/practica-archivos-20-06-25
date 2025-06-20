7d48fd25afb4c348590ba4ec2e3d3e0eeea46396
unknown <drromero@uamv.edu.ni>
2025-06-20 09:53:03 -0600

    practica archivo

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,6 +1,9 @@
 #Programa que copia el contenido de un archivo
-with open("original.txt", "r", encoding="utf-8") as archivo_original: 
-    contenido=archivo_original.read()
 
+#Abrimos el archivo original en modo lectura, lo guardamos en una variable
+with open("original.txt", "r", encoding="utf-8") as archivo_original: 
+    contenido=archivo_original.read() #Hacemos que la varaible lea el archivo y que mande la informacion a la ota variable
+
+#Hacemos que abra el archivo copia y si no existe hacemos que lo cree y se guarde como otra variable
 with open("copia.txt", "w", encoding="utf-8") as archivo_copia:
-    archivo_copia.write(contenido)
+    archivo_copia.write(contenido) #Esa variable escribe el contenido de la otra variable
